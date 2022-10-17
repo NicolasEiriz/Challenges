48ef955810ea176c74d138c2ab31cd8556b1b91d
NicolasEiriz <eiriznicolas@gmail.com>
2022-10-17 19:32:40 -0300

    update

--- First_Challenges_resolved.js
+++ First_Challenges_resolved.js
@@ -188,8 +188,7 @@ array.forEach(element =>{
 result.push(`${number}: ${element}`)
   number += 1 }) 
 return result
-}
- 
+  }
 }
 
 // Link to kata: https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9
