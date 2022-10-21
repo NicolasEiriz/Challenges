5e102c99ee75058831f6d91dc89035b7c2ebfac1
Nicolas Eiriz <eiriznicolas@gmail.com>
2022-10-21 16:44:02 -0300

    Adding a new challenge resolved

--- Perfect-square.js
+++ Perfect-square.js
@@ -14,4 +14,4 @@ function findNextSquare(sq) {
 
 function findNextSquare(122)
 
-Link of kata: https://www.codewars.com/kata/56269eb78ad2e4ced1000013
+//Link of kata: https://www.codewars.com/kata/56269eb78ad2e4ced1000013
